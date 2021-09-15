fdf72fcea781228caf4f77fa7865f5cb1b78ed4a
jbmitchell7 <jbmitchell747@gmail.com>
2021-09-15 11:03:52 -0500

    set up redirect for / to /documentation

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnified
 // GET requests
 //Home screen of API
 app.get('/', (req, res) => {
-    res.status(200).send('Welcome to JakesMovieDB!')
+    res.status(200).redirect('/documentation')
 });
 //get all movies- returns as json
 app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
