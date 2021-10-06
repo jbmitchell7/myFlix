35ee43a0d75238dbb3f1bba5cadf40b6abff6ad0
Jake Mitchell <jbmitchell747@gmail.com>
2021-10-06 16:05:11 -0500

    added password length requirement

--- users.js
+++ users.js
@@ -36,6 +36,7 @@ module.exports = (app) => {
         check('Username', 'Username is required').isLength({ min: 5 }),
         check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
         check('Password', 'Password is required').not().isEmpty(),
+        check('Password', 'Password is required').isLength({ min: 8 }),
         check('Email', 'Email does not appear to be valid').isEmail()
     ], (req, res) => {
 
@@ -76,6 +77,7 @@ module.exports = (app) => {
         check('Username', 'Username is required').isLength({ min: 5 }),
         check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
         check('Password', 'Password is required').not().isEmpty(),
+        check('Password', 'Password is required').isLength({ min: 8 }),
         check('Email', 'Email does not appear to be valid').isEmail()
     ], passport.authenticate('jwt', { session: false }), (req, res) => {
 
